b1e54726e42e2e4ab497ab824d36ee37105bf312
Edson Borin <edson@ic.unicamp.br>
2020-06-07 08:27:12 -0300

    Adding scripts to generate charts with step times per epoch

---
+++ experimental_results/2020-06-FCN-on-sagemaker/plot-charts.sh
@@ -0,0 +1,20 @@
+function fail()
+{
+    echo "FAIL: $@"
+    exit 1
+}
+
+# Read log files from logs directory, extract execution times and organize them on execution-times/VMTYPE/BS/EXP.json
+echo "Plotting charts for results on execution-times/*/*/*.json"
+
+for jfile in execution-times/*/*/*.json; do
+    OUTDIR=`dirname "${jfile}"`
+    EXP=`basename "${jfile}" .json`
+    VMTYPE=`echo "${OUTDIR}" | cut -d/ -f2`
+    BS=`echo "${OUTDIR}" | cut -d/ -f3`
+    OUTFILE="${OUTDIR}/${EXP}.pdf"
+    echo "Processing $jfile => ${OUTFILE}"
+    python3 ./plot-step_times_per_epoch.py -t "${VMTYPE} / ${BS} / ${EXP}" -o "${OUTFILE}" < "${jfile}" || \
+	fail "Error when generating ${OUTFILE}"
+done
+    
