caa4511f1a1071a1cf7ce4475d309c2ab8bc0f38
AlvaroMarques <sc.poi458@gmail.com>
2020-06-19 02:45:49 -0300

    Match utility added in get-log

--- experimental_results/2020-06-FCN-on-sagemaker/get-log-by-stream-name.sh
+++
@@ -1,9 +0,0 @@
-function fail()
-{
-    echo "FAIL: $@"
-    exit 1
-}
-
-# Use -> get-log-by-stream-name.sh {Log stream name} > {Output file}
-
-aws logs get-log-events --log-group-name /aws/sagemaker/TrainingJobs --log-stream-name $1 | python3 get_log_messages.py
