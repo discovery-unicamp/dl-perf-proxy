cdb822162ad088379cf1849fdec5e1d6e5701c72
AlvaroMarques <sc.poi458@gmail.com>
2020-07-11 03:53:21 -0300

    get-log.sh modified to get g4 logs as well

--- cnn-mnist/experimental-results/2020-07-MNIST-on-sagemaker/get-log.sh
+++ cnn-mnist/experimental-results/2020-07-MNIST-on-sagemaker/get-log.sh
@@ -8,16 +8,19 @@ function fail()
 
 if [[ $(echo "$1") == "match" ]]
 then
-	if [[ "$2" =~ mnist\-p[0-9]+\-gpu[0-9]+\-b[0-9]+\-e[0-9]+\/ ]] ; then
+	if [[ "$2" =~ mnist\-[[:alnum:]]+\-gpu[0-9]+\-b[0-9]+\-e[0-9]+\/ ]] ; then
 		var=$(echo "$2" | grep -E -o ".+/")
-		p=$(echo "$var" | grep -E -o "p\d+" | grep -o -E "\d+")
+		p=$(echo "$var" | grep -o -E "mnist-[A-Za-z0-9]+")
+		p=${p/mnist-/}
 		gpu=$(echo "$var" | grep -E -o "gpu\d+" | grep -o -E "\d+")
 		e=$(echo "$var" | grep -E -o "e\d+" | grep -o -E "\d+")
 		b=$(echo "$var" | grep -E -o "b\d+" | grep -o -E "\d+")
-		aws logs get-log-events --log-group-name /aws/sagemaker/TrainingJobs --log-stream-name $2 | python3 read-log-messages.py > logs/result-p$p-$gpu-$b-e$e.txt
+		aws logs get-log-events --log-group-name /aws/sagemaker/TrainingJobs --log-stream-name $2 | python3 read-log-messages.py > logs/result-$p-$gpu-$b-e$e.txt
 	else
 		aws logs get-log-events --log-group-name /aws/sagemaker/TrainingJobs --log-stream-name $2 | python3 read-log-messages.py 
+		echo ""
 	fi
 else
 	aws logs get-log-events --log-group-name /aws/sagemaker/TrainingJobs --log-stream-name $1 | python3 read-log-messages.py 
+	echo ""
 fi
