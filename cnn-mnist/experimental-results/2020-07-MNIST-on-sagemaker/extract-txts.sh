963fa1171a2baf6bb5b5e555e9db10b44a5370ee
AlvaroMarques <sc.poi458@gmail.com>
2020-08-04 01:55:31 -0300

    added script to extract logs from s3 bucket
    Agora como os logs são salvos num arquivo de texto, para cada
    trainig job o Sagemaker cria um diretório no bucket de saída
    com o nome do training job, e dentro do diretório temos outro
    chamado output que dentro deles teremos um tar.gz que contém o
    log em .txt. Esse script carrega o tar.gz para os training jobs
    usados, extrai o arquivo de texto e coloca na pasta logs.

---
+++ cnn-mnist/experimental-results/2020-07-MNIST-on-sagemaker/extract-txts.sh
@@ -0,0 +1,47 @@
+LOGS_DIR="./logs/"
+# p2 instances
+for batch in 256 512;
+	do for g in 1 8 16; 
+		do aws s3 sync s3://fcncloudml/mnist-p2-gpu$g-b$batch-e2/ .; 
+		a="$(tar -ztf output/model.tar.gz '*.txt')";
+		tar -xf output/model.tar.gz $a;
+	       	mv $a $LOGS_DIR/result-p2-$g-$batch-e2.txt;
+		echo $LOGS_DIR/result-p2-$g-$batch-e2.txt;
+	done; 
+done;
+# p3 instances
+for batch in 256 512;
+	do for g in 1 4 8; 
+		do aws s3 sync s3://fcncloudml/mnist-p3-gpu$g-b$batch-e2/ .; 
+		a="$(tar -ztf output/model.tar.gz '*.txt')";
+		tar -xf output/model.tar.gz $a;
+	       	mv $a $LOGS_DIR/result-p3-$g-$batch-e2.txt;
+	       	echo $LOGS_DIR/result-p3-$g-$batch-e2.txt;
+	done; 
+done;
+# g4.xlarge instances
+for batch in 256 512 1024 2048;
+	do aws s3 sync s3://fcncloudml/mnist-g4dnxlarge-gpu1-b$batch-e2/ .; 
+	a="$(tar -ztf output/model.tar.gz '*.txt')";
+	tar -xf output/model.tar.gz $a;
+	mv $a $LOGS_DIR/result-g4dn-1-$batch-e2.txt;
+	echo $LOGS_DIR/result-g4dn-1-$batch-e2.txt;
+done;
+# g4dn.Nxlarge instances with 1 GPU 
+for batch in 256 512 1024 2048;
+	do for N in 2 4 8 16;
+	do aws s3 sync s3://fcncloudml/mnist-g4dn${N}xlarge-gpu1-b$batch-e2/ .; 
+		a="$(tar -ztf output/model.tar.gz '*.txt')";
+		tar -xf output/model.tar.gz $a;
+		mv $a $LOGS_DIR/result-g4dn${N}xlarge-1-$batch-e2.txt;
+		echo $LOGS_DIR/result-g4dn${N}xlarge-1-$batch-e2.txt;
+	done;
+done;
+# g4dn.12xlarge instances
+for batch in 256 512 1024 2048;
+	do aws s3 sync s3://fcncloudml/mnist-g4dn12xlarge-gpu4-b$batch-e2/ .; 
+	a="$(tar -ztf output/model.tar.gz '*.txt')";
+	tar -xf output/model.tar.gz $a;
+	mv $a $LOGS_DIR/result-g4dn-4-$batch-e2.txt;
+	echo $LOGS_DIR/result-g4dn-4-$batch-e2.txt;
+done;
