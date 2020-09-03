d8ad8315a44b87f0ee7d48f99e0e4af39e7eecf3
AlvaroMarques <sc.poi458@gmail.com>
2020-09-03 01:48:09 -0300

    Files renamed and training logs functions added

--- resnet50-cifar/experimental-results/2020-08-CIFAR-on-sagemaker/utils.py
+++ resnet50-cifar/experimental-results/2020-08-CIFAR-on-sagemaker/utils.py
@@ -1,11 +1,21 @@
 import yaml
 from datetime import datetime
-
-def format_name(name,instance, gpu, batch, number, error=False):
+import re
+def format_name_sagemaker(name,instance, gpu, batch, number, error=False):
     if not error:
         return f"{name}-{instance}-gpu{gpu}-b{batch}-e{number}"
     else:
         return f"{name}-{instance}-gpu{gpu}-b{batch}-e{number}-real"
+
+def format_name_logs(name, instance, gpu, batch, number, error=False):
+    return f"result-{instance}-{gpu}-{batch}-e{number}"
+
+def tj_name_to_tuple(tj_name):
+    tj_name = tj_name.replace("-real", "")
+    pattern = re.compile(r"([\w\d]+)-([\w\d]+)-gpu([\d+])-b([\d]+)-e([\d]+)")
+    groups = pattern.match(tj_name).groups()
+    return list(groups)
+
 def yaml_to_tj_names(document):
     """
     reads yaml and return training jobs names according to the training job name pattern used
@@ -25,12 +35,12 @@ def yaml_to_tj_names(document):
             batchs = batchs.replace(" ","") .split(",")
             for gpu in gpus.replace(" ","").split(","):
                 for batch in batchs:
-                    training_jobs_names.append(format_name(name, instance, gpu, batch, number))
+                    training_jobs_names.append(format_name_sagemaker(name, instance, gpu, batch, number))
             if "errors" in yaml_read[experiment]["instances"][instance]:
                 for error in str(yaml_read[experiment]["instances"][instance]["errors"]).replace(" ","").split(";"):
                     gpu, batch = error.split(",")
-                    training_jobs_names.remove(format_name(name, instance, gpu, batch, number))
-                    training_jobs_names.append(format_name(name, instance, gpu, batch, number, True))
+                    training_jobs_names.remove(format_name_sagemaker(name, instance, gpu, batch, number))
+                    training_jobs_names.append(format_name_sagemaker(name, instance, gpu, batch, number, True))
 
     return training_jobs_names
 
@@ -42,6 +52,6 @@ def datetime_json_converter(inp):
         return inp.__str__()
 
 if __name__ == "__main__":
-    with open("start.yaml",'r') as document:
+    with open("e0-low-cost-instances.yaml",'r') as document:
         for i in yaml_to_tj_names(document):
-            print(i)
+            print(tj_name_to_tuple(i))
