149da28c8c0d6fb1f5c69779467e1d235934c439
AlvaroMarques <sc.poi458@gmail.com>
2020-11-21 17:30:03 -0300

    Cost of Sagemaker Training in sagemaker summary added

--- resnet50-cifar/experimental-results/2020-08-CIFAR-on-sagemaker/create-sagemaker-logs.py
+++ resnet50-cifar/experimental-results/2020-08-CIFAR-on-sagemaker/create-sagemaker-logs.py
@@ -13,6 +13,21 @@ yaml_filepath = args.filename
 
 client = boto3.client("sagemaker")
 
+initial_costs = {
+    "ml.p2.xlarge": 0.9,
+    "ml.p2.8xlarge": 7.2,
+    "ml.p2.16xlarge": 14.4,
+    "ml.p3.2xlarge": 3.06,
+    "ml.p3.8xlarge": 12.24,
+    "ml.p3.16xlarge": 24.48,
+    "ml.g4dn.xlarge": 0.526,
+    "ml.g4dn.2xlarge": 0.752,
+    "ml.g4dn.4xlarge": 1.204,
+    "ml.g4dn.8xlarge": 2.176,
+    "ml.g4dn.16xlarge": 4.352,
+    "ml.g4dn.12xlarge": 3.912
+}
+
 
 with open(yaml_filepath, 'r') as yaml_file:
     for tj_name in (yaml_to_tj_names(yaml_file.read())):
@@ -29,8 +44,10 @@ with open(yaml_filepath, 'r') as yaml_file:
             for hyperparameter in describe["HyperParameters"]:
                 summary_file.write(f"\t{hyperparameter}: {describe['HyperParameters'][hyperparameter]}\n")
             billable_time = describe["BillableTimeInSeconds"]
+            cost = initial_costs[instance]/3600 * billable_time
             total_training_time = describe["TrainingTimeInSeconds"]
             summary_file.write(f"billable time: {billable_time}\n")
+            summary_file.write(f"cost: U$ {cost:.4f}\n")
             summary_file.write(f"total training time: {total_training_time}\n") 
             summary_file.write("transitions:\n")
             for secondary_transition in describe['SecondaryStatusTransitions']:
